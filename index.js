179610ec930b66334659f8f0fabbfec57d86f8af
meior <494579004@qq.com>
2017-12-01 17:34:15 +0800

    fix file path and remove folder filter

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fileUtil = require('./utils/fileUtil');
 const path = require('path');
 
+// 题目基础路径
+const base = path.join(__dirname, 'src');
+
 // 保存分类与题目
 let categories = [];
 let questions = [];
@@ -8,12 +11,12 @@ let questions = [];
 // 异步流程处理
 Promise.resolve()
   // 读取分类及数量
-  .then(() => fileUtil.readDir(__dirname))
-  // 排除无关文件夹，对每个类别并行处理Promise流程，以获取题目数量
+  .then(() => fileUtil.readDir(base))
+  // 对每个类别并行处理Promise流程，以获取题目数量
   .then((files) => {
-    categories = files.filter(file => file !== 'node_modules' && file !== 'utils');
-
-    const dirs = categories.map(dir => fileUtil.readDir(path.resolve(__dirname, dir)));
+    categories = files;
+    console.log(categories);
+    const dirs = categories.map(dir => fileUtil.readDir(path.resolve(base, dir)));
     return Promise.all(dirs);
   })
   // 处理并行流程将所有的所有结果，获取题目及数量
