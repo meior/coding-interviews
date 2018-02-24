77bf2cffc11e1d737d9419d991024f355c4fe7b6
meior <494579004@qq.com>
2018-02-24 16:47:19 +0800

    add js doc for function

--- src/backtracking/moving-count/movingCount.js
+++ src/backtracking/moving-count/movingCount.js
@@ -1,3 +1,11 @@
+/**
+ * 构造二维数组
+ *
+ * @param {Number} rows 数组行数
+ * @param {Number} cols 数组列数
+ * @param {Number} initial 元素初始值
+ * @returns {Array} 构造的新数组
+ */
 function matrix(rows, cols, initial) {
   const arr = [];
   for (let i = 0; i < rows; i++) {
@@ -10,6 +18,12 @@ function matrix(rows, cols, initial) {
   return arr;
 }
 
+/**
+ * 计算整数各个数位的数字之和
+ *
+ * @param {Number} number 待计算的整数
+ * @returns {Number} 数位和
+ */
 function getSum(number) {
   let res = 0;
   let num = number || 0;
@@ -20,15 +34,30 @@ function getSum(number) {
   return res;
 }
 
+/**
+ * 回溯法主体
+ *
+ * @param {NUmber} i 横坐标
+ * @param {NUmber} j 纵坐标
+ * @param {Number} rows 矩阵行数
+ * @param {Number} cols 矩阵列数
+ * @param {Array} flag 解向量组
+ * @param {Number} threshold 题目限制条件阀值
+ * @returns {Number} 可到达点的个数
+ */
 function backTrack(i, j, rows, cols, flag, threshold) {
   const flags = flag || [];
+  // 递归出口为整个矩阵的四个边界
+  // 可到达满足两个条件：1.数位和小于阀值，2.该位置未被标记过
   if (i < 0 || i >= rows || j < 0 || j >= cols ||
     getSum(i) + getSum(j) > threshold || flags[i][j] === 1
   ) {
     return 0;
   }
 
+  // 标记当前位置为可到达
   flags[i][j] = 1;
+  // 继续递归上下左右的方格
   return backTrack(i - 1, j, rows, cols, flags, threshold) +
     backTrack(i + 1, j, rows, cols, flags, threshold) +
     backTrack(i, j - 1, rows, cols, flags, threshold) +
@@ -36,6 +65,14 @@ function backTrack(i, j, rows, cols, flag, threshold) {
     1;
 }
 
+/**
+ * 机器人的运动范围，采用回溯法
+ *
+ * @param {Number} threshold 题目限制条件阀值
+ * @param {Number} rows 矩阵行数
+ * @param {Number} cols 矩阵列数
+ * @returns {Number} 可到达点的个数
+ */
 function movingCount(threshold, rows, cols) {
   const flag = matrix(rows, cols, 0);
   return backTrack(0, 0, rows, cols, flag, threshold);
